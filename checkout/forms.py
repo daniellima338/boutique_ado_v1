aac3aaf614c8499577657afd9d6293762d210997
daniellima338 <daniellima338@gmail.com>
2021-12-20 09:46:26 +0000

    added checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Order
 
-class OrderForm(forms.ModelsForm):
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
@@ -14,7 +14,6 @@ class OrderForm(forms.ModelsForm):
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
-
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
@@ -33,7 +32,7 @@ class OrderForm(forms.ModelsForm):
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:
-                placeholder = placeholder[field]
+                placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
