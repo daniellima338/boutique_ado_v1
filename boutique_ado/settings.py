ee4a1f4582f3fd7e598b0bc9de434f09e22923f8
daniellima338 <daniellima338@gmail.com>
2021-12-26 12:53:02 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,6 +185,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control. it tells the browser that it is okay to cache static files. it will improve performance of the site.
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'daniellima338-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -201,7 +207,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
+
 
 
 
