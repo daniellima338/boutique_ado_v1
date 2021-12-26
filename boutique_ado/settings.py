a08f6a0db51e68066b83d864f9255614f02dcfa3
daniellima338 <daniellima338@gmail.com>
2021-12-26 12:38:44 +0000

    059 - deployment part 5 - Setup AWS bucket

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -199,8 +199,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 
